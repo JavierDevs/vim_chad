a8cfa4f1a1da960d5187c1cb8abaab8af501d7cd
pendragons-code <88629349+pendragons-code@users.noreply.github.com>
2022-05-29 00:30:44 +0800

    Update startup.sh
    added line to install latest LTS version of nodejs.

--- startup.sh
+++ startup.sh
@@ -3,6 +3,7 @@ git clone https://github.com/nvm-sh/nvm.git .nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm i --lts
 npm i
 npm update
 node .
