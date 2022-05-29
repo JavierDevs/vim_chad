c08483c7cbd32c899076f4dabf5eb2fb8e4d57f1
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-05-29 15:39:00 +0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,13 @@
 module.exports = (bot, messageCreate) => {
 const Discord = require("discord.js")
-let {prefix, ownerid} =  require("../config.json")
+let {dprefix, ownerid, Default_dual_prefix} =  require("../config.json")
+
+
 if (messageCreate.author.bot || messageCreate.channel.type === 'dm') return;
-    if (messageCreate.content.indexOf(prefix) !== 0){
+    if (messageCreate.content.indexOf(dprefix) !== 0){
       return;
     }
-const args = messageCreate.content.slice(prefix.length).trim().split(/ +/g);
+const args = messageCreate.content.slice(dprefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
 const cmd = bot.commands.get(command) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
 if(!cmd){return}
